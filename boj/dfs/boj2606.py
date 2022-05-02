a64f1e8fea1721f313d149791131dc661e3919a1
zwonlala <s26788761@naver.com>
2022-05-02 22:38:44 +0900

    Update boj2606.py

--- boj/dfs/boj2606.py
+++ boj/dfs/boj2606.py
@@ -1,4 +1,5 @@
 # 220501 22:49 ~ 23:20 1트 틀림.
+# 220502 20:32 ~ 20:41 솔.
 
 n = int(input())
 e = int(input())
@@ -10,13 +11,46 @@ for _ in range(e):
     graph[v1].append(v2)
     graph[v2].append(v1)
 
-# print(graph)
+print(graph)
 
 
 visited = [False] * (n + 1)
 
 def dfs(graph, visited, i):
-    if i < 0 or i >= n:
+    if i < 0 or i > n:
+        return 0
+    
+    if visited[i] == False:
+        visited[i] = True
+        print(i, graph[i]);
+        for j in graph[i]:
+            print(j, graph[i])
+            dfs(graph, visited, j)
+
+dfs(graph, visited, 1)
+
+print(visited)
+print(visited.count(True) - 1)
+    
+
+
+
+# 220502 2nd try
+
+n = int(input())
+e = int(input())
+
+graph = [[] for _ in range(n+1)]
+
+for _ in range(e):
+    v1, v2 = map(int, input().split())
+    graph[v1].append(v2)
+    graph[v2].append(v1)
+
+visited = [False] * (n + 1)
+
+def dfs(graph, visited, i):
+    if i < 0 or i > n:
         return 0
     
     if visited[i] == False:
@@ -25,7 +59,5 @@ def dfs(graph, visited, i):
             dfs(graph, visited, j)
 
 dfs(graph, visited, 1)
-
-# print(visited)
 print(visited.count(True) - 1)
     

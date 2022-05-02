fbf87d2cfb2216cfc061b1d6647e1e27dc58c6f7
zwonlala <s26788761@naver.com>
2022-05-02 23:36:04 +0900

    Add BFS Example 1

---
+++ bfs/bfsEx1.py
@@ -0,0 +1,44 @@
+from collections import deque
+
+n, m = map(int, input().split())
+graph = []
+
+for _ in range(n):
+    graph.append(list(map(int, input())))
+
+# print(graph)
+
+dx = [-1, 0, +1, 0]
+dy = [0, -1, 0, +1]
+
+def bfs(x, y):    
+    queue = deque()
+    queue.append((x, y))
+
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                continue
+
+            if graph[nx][ny] == 0:
+                continue
+
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx, ny))
+
+    return graph[n - 1][m - 1]
+
+print(bfs(0, 0))
+
+
+# 5 6
+# 101010
+# 111111
+# 000001
+# 111111
+# 111111

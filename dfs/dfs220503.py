9ede5729279c1155b2e20ee61048d06ae94e17b8
zwonlala <s26788761@naver.com>
2022-05-03 23:00:12 +0900

    Add dfs220503.py

---
+++ dfs/dfs220503.py
@@ -0,0 +1,23 @@
+def dfs(graph, v, visited):
+    visited[v] = True
+    print(v, end=' ')
+
+    for next in graph[v]:
+        if not visited[next]:
+            dfs(graph, next, visited)
+
+graph = [
+    [],
+    [2, 3, 8],
+    [1, 7],
+    [1, 4, 5],
+    [3, 5],
+    [3, 4],
+    [7],
+    [2, 6, 8],
+    [1, 7]
+]
+
+visited = [False] * 9
+
+dfs(graph, 1, visited)

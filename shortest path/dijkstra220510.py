b6f73a66f34f17e3c7cfb9827ee4b21d18f2122e
zwonlala <s26788761@naver.com>
2022-05-10 22:33:15 +0900

    Add dijkstra220510.py

---
+++ shortest path/dijkstra220510.py
@@ -0,0 +1,61 @@
+import sys
+input = sys.stdin.readline
+INF = int(1e9)
+
+n, m = map(int, input().split())
+start = int(input())
+
+graph = [[] for i in range(n + 1)]
+visited = [False] * (n + 1)
+distance = [INF] * (n + 1)
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))
+
+def get_smallest_node():
+    min_value = INF
+    index = 0
+
+    for i in range(1, n + 1):
+        if distance[i] < min_value and not visited[i]:
+            min_value = distance[i]
+            index = i
+    return index
+
+def dijkstra(start):
+    distance[start] = 0
+    visited[start] = True
+    for j in graph[start]:
+        b, c = j
+        distance[b] = c
+
+    for _ in range(n - 1):
+        now = get_smallest_node()
+        visited[now] = True
+        
+        for j in graph[now]:
+            b, c = j
+            cost = distance[now] + c
+            if cost < distance[b]:
+                distance[b] = cost
+
+dijkstra(start)
+
+for i in range(1, n + 1):
+    print('INFINITY' if distance[i] == INF else distance[i])
+
+
+# 6 11
+# 1
+# 1 2 2
+# 1 3 5
+# 1 4 1
+# 2 3 3
+# 2 4 2
+# 3 2 3
+# 3 6 5
+# 4 3 3
+# 4 5 1
+# 5 3 1
+# 5 6 2

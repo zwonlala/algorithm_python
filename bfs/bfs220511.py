a883739ca85515f0c19e0aa8123256f1d2b5188d
zwonlala <s26788761@naver.com>
2022-05-11 22:55:14 +0900

    Add bfs220511.py

---
+++ bfs/bfs220511.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+def bfs(graph, start, visited):
+    queue = deque([])
+    visited[start] = True
+    queue.append(start)
+
+    while queue:
+        v = queue.popleft()
+        print(v, end=' ')
+
+        for next in graph[v]:
+            if not visited[next]:
+                visited[next] = True
+                queue.append(next)
+
+graph = [
+    [],
+    [2, 3, 8],
+    [1, 7],
+    [1, 4, 5],
+    [3, 5],
+    [3, 4],
+    [7],
+    [2, 6, 8],
+    [1, 7]
+]
+
+visited = [False] * 9
+
+bfs(graph, 1, visited)

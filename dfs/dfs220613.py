502ec6d4320c0c45e593fed3fb0766cbcaba9bc2
zwonlala <s26788761@naver.com>
2022-06-13 14:02:36 +0900

    Add boj220613.py

---
+++ dfs/dfs220613.py
@@ -0,0 +1,29 @@
+import sys
+
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+def dfs(graph, start, visited):
+    visited[start] = True
+    print(start, end=' ')
+
+    for next in graph[start]:
+        if not visited[next]:
+            dfs(graph, next, visited)
+
+graph = [
+    [],
+    [2, 3, 8],
+    [1, 7],
+    [1, 4, 5],
+    [3, 5],
+    [3, 4],
+    [7],
+    [2, 6, 8],
+    [1, 7]
+]
+
+visited = [False] * 9
+
+dfs(graph, 1, visited)
+

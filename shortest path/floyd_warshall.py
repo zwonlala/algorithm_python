b8f325dfdb6f17ca85427ca4c2973710c07a642b
zwonlala <s26788761@naver.com>
2022-06-26 23:11:38 +0900

    Add floyd_warshall.py

---
+++ shortest path/floyd_warshall.py
@@ -0,0 +1,30 @@
+INF = int(1e9)
+
+n = int(input())
+m = int(input())
+
+# 2차원 리스트(그래프)를 만들고, 모든 값을 무한대로 초기화
+graph = [[INF] * (n + 1) for _ in range(n + 1)]
+
+# 자기 자신에게 가는 비용은 0
+for x in range(1, n + 1):
+    graph[x][x] = 0
+
+# 각 간선에 대한 정보를 입력받아, 그 값으로 초기화
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a][b] = c
+    
+for k in range(1, n + 1):
+    for x in range(1, n + 1):
+        for y in range(1, n + 1):
+            graph[x][y] = min(graph[x][y], graph[a][k] + graph[k][b])
+
+for x in range(1, n + 1):
+    for y in range(1, n + 1):
+
+        if graph[x][y] == INF:
+            print("INFINITY", end = ' ')
+        else:
+            print(graph[x][y], end = ' ')
+    print()

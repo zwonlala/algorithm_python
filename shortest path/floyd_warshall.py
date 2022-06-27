b2b27b96ee66448ad02c67bd0c5725a96dfbbaf8
zwonlala <s26788761@naver.com>
2022-06-27 20:16:22 +0900

    Update floyd_warshall.py

--- shortest path/floyd_warshall.py
+++ shortest path/floyd_warshall.py
@@ -18,7 +18,7 @@ for _ in range(m):
 for k in range(1, n + 1):
     for x in range(1, n + 1):
         for y in range(1, n + 1):
-            graph[x][y] = min(graph[x][y], graph[a][k] + graph[k][b])
+            graph[x][y] = min(graph[x][y], graph[x][k] + graph[k][y])
 
 for x in range(1, n + 1):
     for y in range(1, n + 1):
@@ -28,3 +28,14 @@ for x in range(1, n + 1):
         else:
             print(graph[x][y], end = ' ')
     print()
+
+
+# 4
+# 7
+# 1 2 4
+# 1 4 6
+# 2 1 3
+# 2 3 7
+# 3 1 5
+# 3 4 4
+# 4 3 2

72f4c21526db3f3d1b5027dc0589923536cea200
jiwonSong <s26788761@naver.com>
2022-07-17 20:14:27 +0900

    Add dijkstra2_220717.py

---
+++ shortest path/dijkstra2_220717.py
@@ -0,0 +1,55 @@
+import heapq
+import sys
+input = sys.stdin.readline
+
+INF = int(1e9)
+
+N, M = map(int, input().split())
+start = int(input())
+
+graph = [[] for _ in range(N + 1)]
+for _ in range(M):
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))
+
+distance = [INF] * (N + 1)
+
+def dijkstra(start):
+    distance[start] = 0
+    # for b, c in graph[start]:
+    #     distance[b] = c
+
+    q = []
+    heapq.heappush(q, (0, start))
+
+    while q:
+        dist, cur = heapq.heappop(q)
+
+        if dist > distance[cur]:
+            continue
+
+        for b, c in graph[cur]:
+            cost = dist + c
+            if cost < distance[b]:
+                distance[b] = cost
+                heapq.heappush(q, (cost, b))
+
+dijkstra(start)
+
+for i in range(1, N + 1):
+    print('INFINITY' if distance[i] == INF else distance[i])
+
+
+# 6 11
+# 1
+# 1 2 2
+# 1 3 5
+# 1 4 1
+# 2 3 3
+# 2 4 2
+# 3 2 3
+# 3 6 5
+# 4 3 3
+# 4 5 1
+# 5 3 1
+# 5 6 2

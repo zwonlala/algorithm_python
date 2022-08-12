3b6be4b2284e9e1eb3c5e38f60994ddd100948f7
jiwonSong <s26788761@naver.com>
2022-08-12 14:17:20 +0900

    Add sort/count_sort.py

---
+++ sort/count_sort.py
@@ -0,0 +1,11 @@
+array = [7, 5, 9, 0, 3, 1, 6, 2, 9, 1, 4, 8, 0, 5, 2]
+
+# 모든 범위의 수를 포함할 수 있는 리스트 선언하고, 초깃값을 모두 0으로 설정
+count = [0] * (max(array) + 1)
+
+for i in range(len(array)):
+    count[array[i]] += 1
+
+for i in range(len(count)):
+    for j in range(count[i]):
+        print(i, end=' ')

b8150227af2c6d1783b6207ed04f2f46cad9ecf4
zwonlala <s26788761@naver.com>
2022-12-27 19:04:42 +0900

    Add file

--- programmers Lv.2/pro42626.py
+++ programmers Lv.2/pro42626.py
@@ -1,9 +1,9 @@
-# 220623 22:00 ~ 
+# 220611
 
+# 1 try
+# - 정확성 5 / 16
+# - 효율성 0 / 5
 
-
-# 기존 코드..
-'''
 def makeNewScoville(arr):
     l1 = arr[0]
     l2 = arr[1]
@@ -28,59 +28,5 @@ def solution(scoville, K):
         scoville = makeNewScoville(scoville)
         answer += 1
         
+        
     return answer
-'''
-
-
-# 220623 푼거..
-#  정확성 12/16 (57.1), 효율성 5/5 (23.8)
-import heapq
-
-def solution(scoville, K):
-    ANS = 0
-    
-    q = []
-    
-    for s in scoville:
-        heapq.heappush(q, (s, s))
-    
-    while True:
-        min1, _ = heapq.heappop(q)
-        if min1 >= K:
-            break
-        
-        min2, _ = heapq.heappop(q)
-        new = min1 + 2 * min2
-        heapq.heappush(q, (new, new))
-        ANS += 1
-    return ANS
-    
-   
-
-
-# 220623 2nd solution -> SOL!
-import heapq
-
-def solution(scoville, K):  
-    if len(scoville) == 0:
-        return 0
-    ANS = 0
-    q = []
-    
-    for s in scoville:
-        heapq.heappush(q, (s, s))
-    
-    while True:
-        # print(q)
-        if len(q) < 2:
-            return ANS if q[0][0] >= K else -1 # 이 부분이 문제였다...!
-        
-        min1, _ = heapq.heappop(q)
-        if min1 >= K:
-            break
-        
-        min2, _ = heapq.heappop(q)
-        new = min1 + 2 * min2
-        heapq.heappush(q, (new, new))
-        ANS += 1
-    return ANS
